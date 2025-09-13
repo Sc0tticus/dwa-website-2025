14719d9f1df086de1969a576f632e34de23eab53
Prince Sithole <prince.sithole@dwaindia.org>
2025-09-13 09:50:00 +0200

    Add blog page

---
+++ src/app/blog/page.js
@@ -0,0 +1,11 @@
+import GetMoreInfo from "../../components/home/GetMoreInfo";
+import Accreditations from "../../components/home/Accreditations";
+
+export default function BlogPage() {
+  return (
+        <main>
+            <GetMoreInfo />
+            <Accreditations />
+        </main>
+  );
+}
