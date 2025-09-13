01bc016f6fe28f51a03842f448024bc2a356b6fe
Prince Sithole <prince.sithole@dwaindia.org>
2025-09-13 15:27:22 +0200

    Fix layout change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono} from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "../components/Navigation";
 import { ThemeProvider } from "../contexts/ThemeContext";
