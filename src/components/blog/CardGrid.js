2c674fdb7436f5496a2ae2e422c22ea74077b653
Prince Sithole <prince.sithole@dwaindia.org>
2025-09-14 08:01:23 +0200

    Amend cardlayout to set 2 card per row max on wider screens

--- src/components/blog/CardGrid.js
+++ src/components/blog/CardGrid.js
@@ -6,30 +6,6 @@ import { Inter } from "next/font/google";
 // Load Inter font
 const inter = Inter({ subsets: ["latin"] });
 
-const Card = ({ image, title, description, date }) => (
-  <div className={`flex flex-col md:flex-row bg-white shadow-md overflow-hidden w-full ${inter.className}`}>
-    {/* Left image */}
-    <div className="relative md:w-1/2 h-48 md:h-auto">
-      <Image src={image} alt={title} fill className="object-cover" />
-    </div>
-
-    {/* Right content */}
-    <div className="flex flex-col justify-between p-4 md:w-1/2">
-      <div>
-        <h2 className="font-bold text-xl mb-2">{title}</h2>
-        <p className="text-gray-700">{description}</p>
-      </div>
-
-      {/* Button and date */}
-      <div className="flex items-center justify-center mt-4 text-gray-500 text-sm space-x-4">
-        <button className="px-4 py-2 bg-[#7F4592] text-white font-semibold hover:bg-[#693770] transition">
-          View Post
-        </button>
-        <span className="border-l border-gray-300 pl-4">{date}</span>
-      </div>
-    </div>
-  </div>
-);
 
 export default function CardGrid() {
   const cards = [
@@ -57,13 +33,99 @@ export default function CardGrid() {
       description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
       date: "August 20, 2025",
     },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
+    {
+      image: "/images/blogpage/handsingrass.jpg",
+      title: "Lorem ipsum dolor sit amet",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.",
+      date: "August 20, 2025",
+    },
   ];
+
+
+const Card = ({ image, title, description, date }) => (
+  <div className={`flex flex-col md:flex-row bg-white shadow-md overflow-hidden w-full ${inter.className}`}>
+    {/* Left image */}
+    <div className="relative md:w-1/2 h-48 md:h-auto">
+      <Image src={image} alt={title} fill className="object-cover" />
+    </div>
+
+    {/* Right content */}
+    <div className="flex flex-col justify-between p-4 md:w-1/2">
+      <div>
+        <h2 className="font-bold text-xl mb-2">{title}</h2>
+        <p className="text-gray-700">{description}</p>
+      </div>
+
+      {/* Button and date */}
+      <div className="flex items-center justify-center mt-4 text-gray-500 text-sm space-x-4">
+        <button className="px-4 py-2 bg-[#7F4592] text-white font-semibold hover:bg-[#693770] transition">
+          View Post
+        </button>
+        <span className="border-l border-gray-300 pl-4">{date}</span>
+      </div>
+    </div>
+  </div>
+);
 
   return (
     <>
       {/* Card grid constrained by margins */}
       <div className={`w-full px-8 lg:px-32 mt-10 ${inter.className}`}>
-        <div className="grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(500px,1fr))] gap-8 justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 justify-center">
           {cards.map((card, index) => (
             <Card key={index} {...card} />
           ))}
