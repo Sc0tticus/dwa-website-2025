680cf06b2dbe0fa6da5d62c9b29d4d80601a59ad
Prince Sithole <prince.sithole@dwaindia.org>
2025-09-18 18:16:28 +0200

    Add ImpactStats on About page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,6 +2,7 @@ import VisionMission from "../../components/about/VisionMission";
 import HeroSection from "../../components/about/HeroSection";
 import Accreditations from "../../components/home/Accreditations";
 import AboutAccreditations from "../../components/about/AboutAccreditations";
+import ImpactStats from "../../components/about/ImpactStats";
 import GetMoreInfo from "../../components/home/GetMoreInfo";
 
 export default function AboutPage() {
@@ -10,6 +11,11 @@ export default function AboutPage() {
           <HeroSection />
           <VisionMission />
           <AboutAccreditations />
+          <ImpactStats
+            bgColor="bg-gray-50"
+            numbersBg="bg-[#FFD446]"
+          />
+
           <GetMoreInfo />
           <Accreditations />
       </main>
