a808eb96eb73052a46c189b91898bffdd3eb6173
Gahs2 <dayo.abdul@dwaindia.org>
2025-09-19 19:50:32 +0100

    Update projects_gallery.js

--- src/components/resources/projects_gallery.js
+++ src/components/resources/projects_gallery.js
@@ -6,7 +6,7 @@ import pic3 from "/public/images/resources/image/pic3.jpg";
 import pic5 from "/public/images/resources/image/pic5.jpg";
 import styles from "/src/app/resources/styles/resourcesPage.module.css";
 
-export default function featured_stories_component() {
+export default function project_gallery_component() {
 return (
       <div className={styles.projects_gallery}>
         <h2>Projects Gallery</h2>
