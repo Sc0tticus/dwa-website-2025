5a0bb1953154422bd0bf3bff1238dc410186ae80
Gahs2 <dayo.abdul@dwaindia.org>
2025-09-19 15:24:15 +0100

    Update featured_stories.js

--- src/components/resources/featured_stories.js
+++ src/components/resources/featured_stories.js
@@ -1,1 +1,55 @@
-t
+import Link from 'next/link';
+import Image from 'next/image';
+import pic4 from "/public/images/resources/image/pic4.jpg";
+import styles from "/src/app/resources/styles/resourcesPage.module.css";
+
+export default function featured_stories_component() {
+return (
+    <div className={featured_stories}>
+        <h2> Featured Stories</h2>
+        <h4> Stories of Hope & Resilience</h4>
+        <div className={featured_stories_gallery}>
+            <div className={card}> 
+                <div className={picture_holder}>
+                    <Image 
+                      src={pic4.jpg} 
+                      alt="Dalit welfare beneficiaries"
+                    />                     
+                </div>   
+                <div className={stories}>
+                    <p>
+                Livelihoods, micro-credit, and economic empowerment for Dalit and rural families.
+            </p>                   
+                </div>                                                                                      
+          </div>
+          <div className={card}> 
+                <div className={picture_holder}>
+                    <Image 
+                      src={pic4.jpg} 
+                      alt="Dalit welfare beneficiaries"
+                    />                   
+                </div>   
+                <div className={stories}>
+                    <p>
+                Livelihoods, micro-credit, and economic empowerment for Dalit and rural families.
+            </p>                   
+                </div>                                                                                      
+          </div>
+          <div className={card}> 
+                <div className={picture_holder}>
+                    <Image 
+                      src={pic4.jpg} 
+                      alt="Dalit welfare beneficiaries"
+                    />                
+                </div>   
+                <div className={stories}>
+                    <p>
+                Livelihoods, micro-credit, and economic empowerment for Dalit and rural families.
+            </p>                   
+                </div>                                                                                      
+          </div>
+        </div>                     
+    </div>
+);
+}
+
