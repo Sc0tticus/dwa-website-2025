a136d32a59d0aa131a00355cff294c5c92973f1b
Gahs2 <dayo.abdul@dwaindia.org>
2025-09-19 15:58:16 +0100

    Update projects_gallery.js

--- src/components/resources/projects_gallery.js
+++ src/components/resources/projects_gallery.js
@@ -1,1 +1,55 @@
-t
+import Link from 'next/link';
+import Image from 'next/image';
+import pic1 from "/public/images/resources/image/pic1.jpg";
+import pic2 from "/public/images/resources/image/pic2.jpg";
+import pic3 from "/public/images/resources/image/pic3.jpg";
+import pic5 from "/public/images/resources/image/pic5.jpg";
+import styles from "/src/app/resources/styles/resourcesPage.module.css";
+
+export default function featured_stories_component() {
+return (
+      <div className={styles.projects_gallery}>
+        <h2>Projects Gallery</h2>
+        <div className={styles.img_gallery}>
+            <h4>Title 1</h4>
+            <Image 
+              src={pic1.jpg}  
+              alt="Project Image"
+            />
+            <Image 
+              src={pic2.jpg}  
+              alt="Project Image"
+            />
+            <Image 
+              src={pic3.jpg}  
+              alt="Project Image"
+            />
+            <Image 
+              src={pic5.jpg}  
+              alt="Project Image"
+            />
+        </div>
+        <div className={styles.img_gallery}>
+            <h4>Title 2</h4>
+            <Image 
+              src={pic1.jpg}  
+              alt="Project Image"
+            />
+            <Image 
+              src={pic2.jpg}  
+              alt="Project Image"
+            />
+            <Image 
+              src={pic3.jpg}  
+              alt="Project Image"
+            />
+            <Image 
+              src={pic5.jpg}  
+              alt="Project Image"
+            />
+        </div>
+    </div>
+);
+}
+
+  
