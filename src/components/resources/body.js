09acde90d0bfd6ae491ccfbe09f894f5e0ae0857
Gahs2 <dayo.abdul@dwaindia.org>
2025-09-20 15:05:28 +0100

    Create body.js

---
+++ src/components/resources/body.js
@@ -0,0 +1,24 @@
+import header from header.js;
+import dwa_intro from dwa_intro.js;
+import featured_stories from featured_stories.js;
+import projects_gallery from projects_gallery.js;
+import orphanage_oldAge from orphanage_oldAge.js;
+import annual_reports from annual_reports.js;
+import GetMoreInfo_donate from GetMoreInfo_donate.js;
+import footer from footer.js;
+
+
+export default function body_component() {
+return (
+   <body>
+       <header />
+       <dwa_intro />
+       <featured_stories />
+       <projects_gallery />
+       <orphanage_oldAge />
+       <annual_reports />
+       <GetMoreInfo_donate />
+       <footer />
+   </body>
+);
+}
