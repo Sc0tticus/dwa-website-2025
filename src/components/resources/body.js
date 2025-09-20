3d1192e7f0996672f31084a42efb162bc8a30224
Gahs2 <dayo.abdul@dwaindia.org>
2025-09-20 15:34:41 +0100

    Update body.js

--- src/components/resources/body.js
+++ src/components/resources/body.js
@@ -1,11 +1,11 @@
-import header from header.js;
-import dwa_intro from dwa_intro.js;
-import featured_stories from featured_stories.js;
-import projects_gallery from projects_gallery.js;
-import orphanage_oldAge from orphanage_oldAge.js;
-import annual_reports from annual_reports.js;
-import GetMoreInfo_donate from GetMoreInfo_donate.js;
-import footer from footer.js;
+import header from 'header.js';
+import dwa_intro from 'dwa_intro.js';
+import featured_stories from 'featured_stories.js';
+import projects_gallery from 'projects_gallery.js';
+import orphanage_oldAge from 'orphanage_oldAge.js';
+import annual_reports from 'annual_reports.js';
+import GetMoreInfo_donate from 'GetMoreInfo_donate.js';
+import footer from 'footer.js';
 
 
 export default function body_component() {
