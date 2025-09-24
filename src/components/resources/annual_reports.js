dc9131189261073850a96edd31070734965569de
Gahs2 <dayo.abdul@dwaindia.org>
2025-09-24 14:32:18 +0100

    Update annual_reports.js

--- src/components/resources/annual_reports.js
+++ src/components/resources/annual_reports.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from "/src/app/resources/styles/resourcesPage.module.css";
+import styles from "src/app/resources/styles/resourcesPage.module.css";
 
 export default function annual_reports_component() {
 return (
